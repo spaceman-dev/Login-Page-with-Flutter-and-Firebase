a2a1275538a6e894704cac46945beb65b57b5ecd
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-04-05 12:58:04 +0530

    Create, validate and save login form + Firebase android authentication integration

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = new GlobalKey<FormState>();
   String _email;
   String _password;
+  bool validateAndSave(){
+    final form = formKey.currentState;
+    if(form.validate()){
+      form.save();
+      return true;
+    }
+    return false;
+  }
+  void validateAndSubmit()
+  {
+    if(validateAndSave()){
+
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -15,7 +30,29 @@ class _LoginPageState extends State<LoginPage> {
         title: new Text('Flutter Login'),
       ),
       body: new Container(
-        child: new Text('Hello World!'),
+        padding: EdgeInsets.all(16.0),
+        child: new Form(
+          key: formKey,
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new TextFormField(
+                  decoration: new InputDecoration(labelText: 'Email'),
+                  validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+                  onSaved: (value) => _email = value,
+                ),
+                new TextFormField(
+                    decoration: new InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+                  onSaved: (value) => _password = value,
+                ),
+                new ElevatedButton(
+                    child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+                    onPressed: validateAndSubmit,
+                )
+              ],
+            )),
       ),
     );
   }
