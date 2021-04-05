6bee15ffb53856f119c3f882f406edb02e47ac3f
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-04-05 11:43:54 +0530

    Creating new app

---
+++ lib/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email;
+  String _password;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Flutter Login'),
+      ),
+      body: new Container(
+        child: new Text('Hello World!'),
+      ),
+    );
+  }
+}
